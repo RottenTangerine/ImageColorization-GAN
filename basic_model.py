233babbe255a7472c65ab0a8b8c90bc8016183ee
Eden <xiang_xuan@qq.com>
2022-09-18 01:31:50 +0800

    :poop: feat: simple sampling and upsampling network (not GAN)

---
+++ basic_model.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        resnet = models.resnet18()
+        self.midlevel_resnet = nn.Sequential(*list(resnet.children())[0:6])  # feature size 128
+        # Upsampling Network
+        self.upsample = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1),
+            nn.Upsample(scale_factor=2)
+        )
+
+    def forward(self, input):
+        midlevel_features = self.midlevel_resnet(input)
+        output = self.upsample(midlevel_features)
+        return output
+
+
