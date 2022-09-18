2939b3f3d9e99c281d0406c1c79acbc56fe5d31c
Eden <xiang_xuan@qq.com>
2022-09-18 15:42:20 +0800

    :bug: fix: Fix the bug caused by inconsistent color mode when reading images

--- MyDataset.py
+++ MyDataset.py
@@ -22,7 +22,7 @@ class ColorizeData(Dataset):
         return len(self.image_list)
 
     def __getitem__(self, index:int):
-        image = Image.open(os.path.join(self.file_path, self.image_list[index]))
+        image = Image.open(os.path.join(self.file_path, self.image_list[index])).convert('RGB')
         data = self.train_data_transform(image)
         target = self.target_data_transform(image)
 
@@ -30,4 +30,11 @@ class ColorizeData(Dataset):
 
 
 if __name__ == '__main__':
+    from torch.utils.data import DataLoader, random_split
     dataset = ColorizeData('landscape_images')
+    split_ratio = 0.2
+    train_dataset, validate_dataset = random_split(dataset,
+                                                   [l := round(len(dataset) * (1 - split_ratio)), len(dataset) - l])
+    train_loader = DataLoader(dataset=train_dataset, batch_size=8, shuffle=True)
+    validate_loader = DataLoader(dataset=validate_dataset, batch_size=8, shuffle=True)
+
